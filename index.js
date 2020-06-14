5944d669427370895e69555a76993daad9ce748d
KimDongMin <rlaehdals753@naver.com>
2020-06-14 18:31:27 +0900

    Node.js: Logout 기능

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const port = 5000; // 아무렇게나 해도 된다.
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const config = require("./config/key");
-const { auth } = require('./middleware/auth');
+const { auth } = require("./middleware/auth");
 
 const { User } = require("./models/User");
 
@@ -82,20 +82,28 @@ app.post("/login", (req, res) => {
 
 //role 0->일반유저 role 0이아니면 관리자
 
-app.get('/api/users/auth', auth , (req,res) => {
-
+app.get("/api/users/auth", auth, (req, res) => {
   //여기 가지 미들웨어를 통과해 왔다는 얘기는 Authentication이 True 라는 말.
   res.status(200).json({
     _id: req.user._id,
     isAdmin: req.user.role === 0 ? false : true,
     isAuth: true,
-    email:req.user.email,
-    name:req.user.name,
-    lastname:req.user.lastname,
-    role:req,user.role,
-    image: req.user.image
-  })
-})
+    email: req.user.email,
+    name: req.user.name,
+    lastname: req.user.lastname,
+    role: req.user.role,
+    image: req.user.image,
+  });
+});
+
+app.get("/api/users/logout", auth, (req, res) => {
+  User.findOneAndUpdate({ _id: req.user._id }, { token: "" }, (err, user) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).send({
+      success: true,
+    });
+  });
+});
 
 app.listen(port, () =>
   console.log(`Example app listening at http://localhost:${port}`)
