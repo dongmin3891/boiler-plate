e894d7b6236c17da3e3b22427fd37d04ada958c0
KimDongMin <rlaehdals753@naver.com>
2020-06-24 15:30:40 +0900

    RegisterPage.js: Modify

--- client/src/components/views/RegisterPage/RegisterPage.js
+++ client/src/components/views/RegisterPage/RegisterPage.js
@@ -3,20 +3,26 @@ import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { loginUser } from "../../../_actions/user_action";
 
-
-
 function RegisterPage(props) {
   const dispatch = useDispatch();
 
   const [Email, setEmail] = useState("");
+  const [Name, setName] = useState("");
   const [Password, setPassword] = useState("");
+  const [ConfirmPassword, setConfirmPassword] = useState("");
 
   const onEmailHandler = (event) => {
     setEmail(event.currentTarget.value);
   };
-
+  const onNameHandler = (event) => {
+    setName(event.currentTarget.value);
+  };
   const onPasswordHandler = (event) => {
     setPassword(event.currentTarget.value);
+  };
+
+  const onConfirmPasswordHandler = (event) => {
+    setConfirmPassword(event.currentTarget.value);
   };
 
   const onSubmitHandler = (event) => {
@@ -36,8 +42,6 @@ function RegisterPage(props) {
     });
   };
 
-
-function RegisterPage() {
   return (
     <div
       style={{
@@ -56,15 +60,19 @@ function RegisterPage() {
         <input type="email" value={Email} onChange={onEmailHandler} />
 
         <label>Name</label>
-        <input type="password" value={Password} onChange={onPasswordHandler} />
+        <input type="text" value={Name} onChange={onNameHandler} />
 
         <label>Password</label>
         <input type="password" value={Password} onChange={onPasswordHandler} />
-        
+
         <label>Confirm Password</label>
-        <input type="password" value={Password} onChange={onPasswordHandler} />
+        <input
+          type="password"
+          value={ConfirmPassword}
+          onChange={onConfirmPasswordHandler}
+        />
         <br />
-        <button>Login</button>
+        <button type="submit">회원가입</button>
       </form>
     </div>
   );
